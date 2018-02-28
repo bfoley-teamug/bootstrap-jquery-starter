dbef7ebf3e98c0fc1af60dac53c819d3acbb1329
bfoley-teamug <withtechron@gmail.com>
2018-02-28 15:39:58 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+//
+
+$( "#target" ).click(function() {
+  console.log("Don't lean on me man cause you ain't got time to check it, You know my Suffragette City, Is outta sight she's all right");
+});
